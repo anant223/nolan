59214cffdcb1a08328003417bad2ec9a657d38df
Anant <p.anant223@gmail.com>
2024-11-13 12:57:04 +0530

    fixing bug

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ const config = {
   appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
   appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
   appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-  openApiKey: String(import.meta.env.VITE_OPEN_API_KEY),
+  openApiKey: String(import.meta.env.VITE_API_KEY),
 };
 
 export default config;
